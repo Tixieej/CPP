7282d68fce5cd2895ed399e1c65ea7893edbb4b9
Tixieej <rixtdieuwke@live.nl>
2020-09-03 16:07:18 +0200

    ex04 done?

--- m01/ex04/ex04.cpp
+++ m01/ex04/ex04.cpp
@@ -6,14 +6,19 @@
 /*   By: rde-vrie <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/03 14:40:10 by rde-vrie      #+#    #+#                 */
-/*   Updated: 2020/09/03 14:42:59 by rde-vrie      ########   odam.nl         */
+/*   Updated: 2020/09/03 16:07:01 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int		main(void)
 {
+	std::string s = "HI THIS IS BRAIN";
+	std::string *ptr = &s;
+	std::string &ref = s;
 
+	std::cout << *ptr << std::endl << ref << std::endl;
+	return (0);
 }
-
