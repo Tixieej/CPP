9ce834f4c3b29400763405fa0c70db0e46d061f6
Rixt <rixtdieuwke@live.nl>
2020-11-01 09:16:53 +0100

    merged the two files

--- m03/ex00/FragTrap.cpp
+++ m03/ex00/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: rixt <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/27 14:47:43 by rixt          #+#    #+#                 */
-/*   Updated: 2020/10/27 16:31:48 by rixt          ########   odam.nl         */
+/*   Updated: 2020/11/01 09:15:15 by rde-vrie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 FragTrap::FragTrap(std::string name)
 {
-	std::cout << "constructed" << std::endl;
+	std::cout << "FR4G-TP is constructed" << std::endl;
 	this->HP = 100;
 	this->maxHP = 100;
 	this->energy = 100;
@@ -34,7 +34,7 @@ FragTrap::FragTrap(FragTrap const &fragtrap)
 
 FragTrap::~FragTrap()
 {
-	std::cout << "destructed" << std::endl;
+	std::cout << "FR4G-TP is destructed" << std::endl;
 }
 
 void FragTrap::operator=(FragTrap const &fragtrap)
@@ -45,20 +45,23 @@ void FragTrap::operator=(FragTrap const &fragtrap)
 
 rangedAttack(std::string const & target)
 {
-	std::cout << "FR4G-TP " << <name> << " attacks " << <target> << " at range, causing " << <damage> << " points of damage!" << std::endl;
+	std::cout << "FR4G-TP " << this->name << " attacks " << target << " at range, causing " << this->rangedAtkDmg << " points of damage!" << std::endl;
 }
 
 meleeAttack(std::string const & target)
 {
-	std::cout << "FR4G-TP " << <name> << " attacks " << <target> << " from nearby, causing " << <damage> << " points of damage!" << std::endl;
+	std::cout << "FR4G-TP " << this->name << " attacks " << target << " from nearby, causing " << this->meleeAtkDmg << " points of damage!" << std::endl;
 }
 
 takeDamage(unsigned int amount)
-{
-
+{	
+	int damage = amount - this->armorDmgRed;
+	this->HP -= damage;
+	std::cout << "Enemy attacks FR4G-TP " << this->name << " with " << amount << " of damage, causing " << damage << " points of damage!" << std::endl;
 }
 
 beRepaired(unsigned int amount)
 {
-
+	this->HP += amount;
+	std::cout << "FR4G-TP " << this->name << " is being repaired for " << amount << " hp, causing " << this->HP << " Health points!" << std::endl;
 }
