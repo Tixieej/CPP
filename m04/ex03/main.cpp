834347d7e1cbe1eeb781f38404935545eab659cd
Tixieej <rixtdieuwke@live.nl>
2020-11-20 21:55:00 +0100

    added pretty colours, thanks Angi :3

--- m04/ex03/main.cpp
+++ m04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rixt <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/12 12:21:00 by rixt          #+#    #+#                 */
-/*   Updated: 2020/11/12 16:31:31 by rixt          ########   odam.nl         */
+/*   Updated: 2020/11/20 21:53:23 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 #include "MateriaSource.hpp"
 #include "ICharacter.hpp"
 #include "IMateriaSource.hpp"
+#include "colors.hpp"
 
 int main()
 {
+	std::cout << COLOR_YELLOW;
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -40,6 +42,6 @@ int main()
 	delete bob;
 	delete me;
 	delete src;
-
+	std::cout << COLOR_RESET;
 	return 0;
 }
