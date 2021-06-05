74412694b640b07dde3625ceeb3b7d32693b4150
Rixt <rixtdieuwke@live.nl>
2021-06-05 17:11:27 +0200

    start of m05, skeleton of the hpp and cpp files

--- m05/ex00/Bureaucrat.cpp
+++ m05/ex00/Bureaucrat.cpp
@@ -6,8 +6,64 @@
 /*   By: rde-vrie <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/05 15:30:34 by rde-vrie      #+#    #+#                 */
-/*   Updated: 2021/06/05 15:30:37 by rde-vrie      ########   odam.nl         */
+/*   Updated: 2021/06/05 17:07:23 by rde-vrie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+#include <iostream>
 
+Bureaucrat::Bureaucrat(void)
+{
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy)
+{
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+}
+
+Bureaucrat			&Bureaucrat::operator=(Bureaucrat const &rhs)
+{
+	this->_type = rhs.getType();
+	this->_xp = rhs.getXP();
+	return (*this);
+}
+
+std::string const	&Bureaucrat::GetName(void) const
+{
+	return (this->_name);
+}
+
+int const			&Bureaucrat::GetGrade(void) const
+{
+	return (this->_grade);
+}
+
+std::string			Bureaucrat::GradeTooHighException(int grade)
+{
+
+}
+
+std::string			Bureaucrat::GradeTooLowException(int grade)
+{
+
+}
+
+void				Bureaucrat::increment()
+{
+	(this->_grade)--;
+}
+
+void				Bureaucrat::increment()
+{
+	(this->_grade)++;
+}
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &bC)
+{
+	o << bC.getName();
+	return (o);
+}
--- m05/ex00/Bureaucrat.hpp
+++ m05/ex00/Bureaucrat.hpp
@@ -6,8 +6,33 @@
 /*   By: rde-vrie <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/05 15:30:25 by rde-vrie      #+#    #+#                 */
-/*   Updated: 2021/06/05 15:30:27 by rde-vrie      ########   odam.nl         */
+/*   Updated: 2021/06/05 17:09:20 by rde-vrie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_H
+# define BUREAUCRAT_H
+# include <string>
+# include <iostream>
 
+class Bureaucrat
+{
+	private:
+		std::string	_name;
+		int			_grade;
+
+	public:
+		Bureaucrat(void);
+		Bureaucrat(Bureaucrat const &);
+		virtual ~Bureaucrat(void);
+		Bureaucrat			&operator=(Bureaucrat const &);
+		std::string const	&getName() const;
+		int	const			&getGrade() const;
+//		std::string			GradeTooHighException(int grade); //misschien worden dit geen methodes.. geen idee nog
+//		std::string			GradeTooLowException(int grade);
+		void				increment();
+		void				decrement();
+};
+	std::ostream		&operator<<(std::ostream &o, Bureaucrat const &bC);
+
+#endif
