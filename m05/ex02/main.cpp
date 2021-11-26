515ee68288b7a3ae271941af60128b13ca436bac
Tixieej <rixtdieuwke@live.nl>
2021-11-26 22:06:03 +0100

    begin of tests in main. the forms work, but the output needs to be changed.

--- m05/ex02/main.cpp
+++ m05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rixt <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/12 12:21:00 by rixt          #+#    #+#                 */
-/*   Updated: 2021/11/26 14:33:42 by rde-vrie      ########   odam.nl         */
+/*   Updated: 2021/11/26 22:03:49 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 int	main()
 {
-	// Constructor too high grade tests
+	// Constructors
 	std::cout << COLOR_PINK;
 	Bureaucrat anne("Anne", 55);
 	ShrubberyCreationForm scf("target scf");
@@ -27,16 +27,100 @@ int	main()
 	PresidentialPardonForm ppf("target ppf");
 	// Constructor too low grade tests
 	//std::cout << COLOR_ORANGE;
-	anne.executeForm(scf);
-	anne.signForm(scf);
-	anne.executeForm(scf);
+
+	try
+	{
+		anne.executeForm(scf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		anne.signForm(scf);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	try
+	{
+		anne.executeForm(scf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
 
 	// Good Bureaucrat, Increment tests
 	std::cout << COLOR_ORANGE;
 	
+	try
+	{
+		anne.executeForm(rrf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		anne.signForm(rrf);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	try
+	{
+		anne.executeForm(rrf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+
 	// Good Bureaucrat, Decrement tests
 	std::cout << COLOR_GREEN;
 	
+	try
+	{
+		anne.executeForm(ppf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		anne.signForm(ppf);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	try
+	{
+		anne.executeForm(ppf);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+
 	// Form sign tests
 	std::cout << COLOR_BLU;
 		
